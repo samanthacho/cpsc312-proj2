a65d13f2cada2cc9f1cd0a66ca751544ae342d3f
Srijon Saha <srijon365@gmail.com>
2019-12-04 13:25:02 -0800

    Add comments

--- request.pl
+++ request.pl
@@ -1,7 +1,11 @@
+% This file contains the API connection for this program. It is responsible for making calls to the Yelp API
+% Bing Translate API, and translating the response into a format which is usable for the caller classes.
+
 :- use_module(library(http/http_client)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/json_convert)).
 
+% json object representing the response structure of the Translate API
 :- json_object translationReqObj(text:string).
 
 translation_api_url(Lang, URL) :-
@@ -10,12 +14,19 @@ translation_api_url(Lang, URL) :-
 translation_api_key(Key) :-
     getenv("TRANSLATION_API_KEY", Key).
 
+% returns true if Response is the json response returned by the Bing Translate API
+% Query is the string to be translated
+% Lang is the language to translate to i.e. English Lang = "en"
 make_translation_request(Query, Lang, Response) :-
     translation_api_url(Lang, URL),
     translation_api_key(Key),
     prolog_to_json(translationReqObj(Query), Body),
     http_post(URL, json([Body]), Response, [request_header("Ocp-Apim-Subscription-Key"=Key)]).
 
+% returns true if Result and Lang are in the Translate json response
+% json reponse must match the expect response
+% Lang is the detected language of the text being translated
+% Result is the translated text
 parse_translation_response([json([detectedLanguage=json([language=Lang, score=_]), translations=[json([text=Result,  to=_])]])], Result, Lang).
 
 yelp_api_url("https://api.yelp.com/v3/businesses/search?").
@@ -23,31 +34,46 @@ yelp_api_url("https://api.yelp.com/v3/businesses/search?").
 yelp_api_key(Key) :-
     getenv("YELP_API_KEY", Key).
 
+% returns True if ParamsStr is the paramtized string part of the URL formed from Params
+% Params is of the form [name='XXX', rating='YYY', ...]
+% ParamStr is of the form 'name=XXX&rating=YYY'
 concat_params(Params, ParamsStr) :-
     atomics_to_string(Params, "&", ParamsStr).
 
+% returns True if ParamURL is the entire URL to make the Yelp API request
+% Params is of the form [name='XXX', rating='YYY', ...]
+% ParamURL is of the form "https://api.yelp.com/v3/businesses/search?name=XXX&rating=YYY&location=Vancouver"
 param_url(Params, ParamURL) :-
     yelp_api_url(URL),
     concat_params(Params, ParamsStr),
     atom_concat(URL, ParamsStr, Part),
     atom_concat(Part, "&location=vancouver", ParamURL).
 
+% returns True if Response is the json response returned by the Yelp API
+% Query is of the form [name='XXX', rating='YYY', ...]
 make_yelp_search_request(Query, Response) :-
     param_url(Query, URL),
     yelp_api_key(Key),
     http_get(URL, Response, [request_header("Authorization"=Key)]).
 
+% returns true if Result is parsed list of businesses from the Yelp API json response
+% Result is list of businesses and each busiensss is of the form [name="XXX", URL="YYY", ...]
 parse_yelp_search_response(json([businesses=Businesses, _=_, _=_]), Result) :-
     once(parse_businesses(Businesses, Result)).
 
+% returns True if Result has all the businessses parsed from the json response
 parse_businesses([], []).
 parse_businesses([B | Businesses], [H | T]) :-
     parse_business(B, H),
     parse_businesses(Businesses, T).
 
+% returns True if Result has a single business is parsed from the json response
 parse_business(json(Attributes), Result) :-
     parse_business_helper(Attributes, Result).
 
+% returns true if all Result has the properties name, rating, price, phone url, or location of the business
+% Input is similar to result but has other unneeded properties as well
+% Result is of the form [name='XXX', rating='YYY', ....]
 parse_business_helper([], []).
 parse_business_helper([name=Name | L1], [name=Name | L2]) :-
     parse_business_helper(L1, L2).
@@ -66,6 +92,8 @@ parse_business_helper([Term=_ | L1], L2) :-
     \+ member(Term, [name, rating, price, display_phone, url]),
     parse_business_helper(L1, L2).
 
+% returns true if Location is parsed from the properties
+% Locaion is a string that is the location of the business
 parse_location([], "").
 parse_location([address1=Location | _], Location).
 parse_location([att=_ | T], Location) :-
--- search.pl
+++ search.pl
@@ -1,20 +1,27 @@
+% This file calls the predicates from the request file and returns readable results for the user
 :- use_module(request).
 :- use_module(translation).
 
+% returns True if Result has the returned parsed response of the Yelp API
+% Params is of the form [name='XXX', rating='YYY', ...]
+% Result is list of businesses and each busiensss is of the form [name="XXX", URL="YYY", ...]
 search(Params, Result) :-
     make_yelp_search_request(Params, Res),
     parse_yelp_search_response(Res, Result).
 
+% returns True if the Resuls are printed in language Lang
 display_results(Result, Lang) :-
     nl(),
     print_businesses(Result, Lang).
 
+% returns true if all the businesses are printed in language Lang
 print_businesses([], _).
 print_businesses([B | Businesses], Lang) :-
     print_business(B, Lang),
     nl(),
     print_businesses(Businesses, Lang).
 
+% returns true if a single business is printed in language Lang
 print_business([], _).
 print_business([name=Name | L1], Lang) :-
     translate("Name", Lang, _, Translation),
