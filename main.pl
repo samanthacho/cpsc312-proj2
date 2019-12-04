286cd941ebd83958f8ce4ed467b7f53473956a25
Samantha Cho <samantha.cho@alumni.ubc.ca>
2019-12-04 13:23:26 -0800

    added comments to main file

--- main.pl
+++ main.pl
@@ -6,13 +6,23 @@
 % Main user function
 q() :-
     write("Let's find some food! Ask me: "),
+		% flush the buffer
     flush_output(current_output),
+		% read the current user input
     readln(Ln),
+		% takes the user input and converts it to a string
     atomics_to_string(Ln, " ", Str),
+		% translates the user input to english and saves the detected language
     translate(Str, "en", DetectedLang, Translation),
+		% takes the translated (english) restult and converts it to a string
     atomics_to_string(TranslationLst, " ", Translation),
+		% changes the string to all lower case letters (needed by the knowledge base)
 	  maplist(downcase_atom, TranslationLst, TranslationLowercase),
+		% parses the parameters for the user input
+		% uses once query to only get 1 result
     once(question(TranslationLowercase, End, Ans)),
     member(End, [[]]),
+		% uses the parsed parameters to query the Yelp API for a satisfying result
     search(Ans, Result),
+		% takes the Result returned and parses the JSON into a user friendly format
     display_results(Result, DetectedLang).
